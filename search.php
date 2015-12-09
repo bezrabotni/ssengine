e68e4ecdc47a05e9a7d484a87050a26d3170a225
Haytham Mones <haythameyd@gmail.com>
2015-12-09 15:27:58 +0100

    added comments to the code

--- search.php
+++ search.php
@@ -12,17 +12,16 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
+
   </head>
   <body background="img/bg.jpg">
+    <!-- NAVIGATION BAR
+    This is where we put the links on top
+    or maybe some modals in the future
+    -->
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
@@ -45,6 +44,12 @@
 
 
     <div class="row" style="margin-top:50px;margin-left:10px;">
+      <!--
+      SEARCH FILTER SECTION
+      This section will contain all the websites to be searched, twitter settings
+      and all the other filters that can be applied to a specific searchsite
+
+      -->
       <div class"col-md-4" style="color:white;">
         <form class="navbar-form navbar-left" role="search" method="post" action="search.php">
           <div class="form-group">
@@ -60,6 +65,15 @@
           <button type="submit" class="btn btn-default">Apply Criteria</button>
         </form>
       </div>
+
+
+
+      <!-- SEARCH RESULTS DISPLAY
+      This Area searches a specific website for
+      the keywords specified
+      It cascades the results based on what has been searched
+      It displays only the results with the links
+      It uses a function search site from the search php file-->
       <div class="col-md-8" style="background-color:white;">
     <?php
     include 'functions.php';
